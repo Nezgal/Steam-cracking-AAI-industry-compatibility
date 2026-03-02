f2d18db38a64f2efc3aa2eaefd382d961c7da6c2
Nezgal <nezgal@protonmail.com>
2026-03-02 14:53:13 +0100

    icons and recipe

---
+++ data-final-fixes.lua
@@ -0,0 +1,18 @@
+data.raw.recipe["basic-oil-processing"].icon = "__Steam-cracking-AAI-industry-compatibility__/graphics/basic-oil-processing.png"
+data.raw.recipe["oil-processing-heavy"].icon = "__Steam-cracking-AAI-industry-compatibility__/graphics/crude-oil-processing.png"
+data.raw.recipe["advanced-oil-processing"].icon = "__Steam-cracking-AAI-industry-compatibility__/graphics/advanced-oil-processing.png"
+
+data.raw.recipe["basic-oil-processing"].ingredients = {
+    {type="fluid", name="steam", amount=50},
+    {type="fluid", name="crude-oil", amount=100}
+}
+
+data.raw.recipe["oil-processing-heavy"].ingredients = {
+    {type="fluid", name="steam", amount=10},
+    {type="fluid", name="crude-oil", amount=100}
+}
+
+data.raw.recipe["advanced-oil-processing"].ingredients = {
+    {type="fluid", name="steam", amount=50},
+    {type="fluid", name="crude-oil", amount=100}
+}
